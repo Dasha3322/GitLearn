618e88d6c9c98f24c4e38f5ef7fb737cc77950b6
Dasha3322 <166533809+Dasha3322@users.noreply.github.com>
2024-07-04 21:04:34 +0300

    feat: hhh

---
+++ script.js
@@ -0,0 +1,49 @@
+// console.log('Hello world');
+// let str = 'Hello world';
+// console.log(str);
+
+// let number = 5;
+// let und;
+// console.log(und);
+// let nullValue = null;
+// console.log(nullValue);
+// let bullValue = true;
+
+// console.log(typeof bullValue);
+
+// var name = 'имя';
+// console.log(name);
+// const pi = 3.14;
+// const sym = Symbol('1');
+// console.log(sym);
+
+// console.log(document);
+// console.log(window);
+
+// alert('Привет');
+// prompt('введите значение');
+// confirm('вам есть 18?');
+// 15 + 15 сложение;
+// 15 - 15 вычитание;
+// 15 * 15 умножение;
+
+// console.log(15**2) возведение в степень;
+
+// console.log(10 % 3); остаток от деления
+// let x = 10;
+// console.log(++x); // инкремент
+// console.log(--x); // декремент
+// console.log(-x); // отрицательное значение
+
+// let x = 5;
+// let y = '10';
+// console.log(x + +y); // добавив + к y без пробела произойдет сложение 15, без + перед y будет 510
+
+// console.log(x - y); // будет -5
+// console.log (x / y);
+// console.log (x * y);
+// console.log (x ** y);
+
+const radius = prompt('введите радиус окружности', ''); // чтобы привети эту строку в число надо перед prompt поставить + без пробела
+const square = 3.14 * radius ** 2;
+alert('площадь окружности' + square); // домашки
